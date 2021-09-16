cb6f5bf2f1583703b9a80cc2e15e4ff6ad662b66
Takudzwa30 <34072789+Takudzwa30@users.noreply.github.com>
2021-09-16 20:06:28 +0400

    Add files via upload

---
+++ tipcalc.js
@@ -0,0 +1,13 @@
+
+
+const bill = 275;
+let tip;
+
+bill >=50 && bill <=300 ? tip = bill * 0.15 :
+tip = bill * 0.2;
+
+let total = bill + tip;
+
+console.log('The bill was ' +bill+ ', the tip was, ' +tip+ ' and the total value is '+total);
+
+
